917766d2cb83ef16c9217fac363d350f6cc1f913
jv-vogler <jvsvogler@gmail.com>
2022-10-18 19:44:18 -0300

    Add hamburger menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import "./style.css";
 
 document.addEventListener("DOMContentLoaded", () => {
-  
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
 });
 
